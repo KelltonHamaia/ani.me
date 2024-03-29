6ba0f0040a015b62185cf50360cfb6f1efd17c14
kellton hamaia <hamaia.kell@gmail.com>
2024-03-29 14:13:22 -0300

    fix conflit app component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,18 @@ import { Header } from "@/components/header/header";
 import { LastNews } from "@/components/last-news/last-news";
 import { LastUpdates } from "@/components/last-updates/last-updates";
 import { RecentAnimes } from "@/components/recent-animes/recent-animes";
+import { Copyright } from "@/components/copyright/copyright";
 
 const Page = () => {
-
     return (
-        <div className='container min-h-screen mx-auto lg:max-w-screen-xl md:max-w-2xl py-5'>
+        <div className='container min-h-screen mx-auto lg:max-w-screen-xl md:max-w-2xl'>
             <Header />
             <LastNews />
             <LastUpdates />
             <RecentAnimes />
+            <Copyright />
         </div>
     );
-
 }
 
 export default Page;
