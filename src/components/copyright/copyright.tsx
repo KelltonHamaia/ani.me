be34071aa833d6ee7ebdf438b9dcc18786e8d543
kellton hamaia <hamaia.kell@gmail.com>
2024-03-29 14:12:29 -0300

    feat: create copyright footer component

---
+++ src/components/copyright/copyright.tsx
@@ -0,0 +1,7 @@
+export const Copyright = () => {
+    return (
+        <footer className='w-full my-5 lg:text-sm md:text-lg text-xs font-normal text-center text-muted-foreground pt-4'>
+            Todos os direitos reservados aos criadores e produtores dos animes
+        </footer>
+    );
+}
